31a0d71969e5203e947dd8bafa0a31c125a70972
HumorA <sk4698025@gmail.com>
2021-02-02 17:58:22 +0800

    加入專案檔案。

---
+++ HardCodeTennis/Tennis.cs
@@ -0,0 +1,85 @@
+﻿
+namespace HardCodeTennis
+{
+    public class Tennis
+    {
+        private int _firstPlayerScoreTimes;
+        private int _secondPlayerScoreTimes;
+
+        public string Score()
+        {
+            if (_firstPlayerScoreTimes == 0 && _secondPlayerScoreTimes == 0)
+            {
+                return "love all";
+            }
+            if (_firstPlayerScoreTimes == 1 && _secondPlayerScoreTimes == 0)
+            {
+                return "fifteen love";
+            }
+            if (_firstPlayerScoreTimes == 2 && _secondPlayerScoreTimes == 0)
+            {
+                return "thirty love";
+            }
+            if (_firstPlayerScoreTimes == 3 && _secondPlayerScoreTimes == 0)
+            {
+                return "forty love";
+            }
+            if (_firstPlayerScoreTimes == 0 && _secondPlayerScoreTimes == 1)
+            {
+                return "love fifteen";
+            }
+            if (_firstPlayerScoreTimes == 0 && _secondPlayerScoreTimes == 2)
+            {
+                return "love thirty";
+            }
+            if (_firstPlayerScoreTimes == 0 && _secondPlayerScoreTimes == 3)
+            {
+                return "love forty";
+            }
+            if (_firstPlayerScoreTimes == 1 && _secondPlayerScoreTimes == 1)
+            {
+                return "fifteen all";
+            }
+            if (_firstPlayerScoreTimes == 2 && _secondPlayerScoreTimes == 2)
+            {
+                return "thirty all";
+            }
+            if (_firstPlayerScoreTimes == 3 && _secondPlayerScoreTimes == 3)
+            {
+                return "deuce";
+            }
+            if (_firstPlayerScoreTimes == 4 && _secondPlayerScoreTimes == 4)
+            {
+                return "deuce";
+            }
+            if (_firstPlayerScoreTimes == 4 && _secondPlayerScoreTimes == 3)
+            {
+                return "Sam adv";
+            }
+            if (_firstPlayerScoreTimes == 3 && _secondPlayerScoreTimes == 4)
+            {
+                return "Joe adv";
+            }
+            if (_firstPlayerScoreTimes == 5 && _secondPlayerScoreTimes == 3)
+            {
+                return "Sam win";
+            }
+            if (_firstPlayerScoreTimes == 3 && _secondPlayerScoreTimes == 5)
+            {
+                return "Joe win";
+            }
+
+            return null;
+        }
+
+        public void FirstPlayerScore()
+        {
+            _firstPlayerScoreTimes++;
+        }
+
+        public void SecondPlayerScore()
+        {
+            _secondPlayerScoreTimes++;
+        }
+    }
+}
---
+++ HardCodeTennisTests/TennisTests.cs
@@ -0,0 +1,155 @@
+﻿using HardCodeTennis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HardCodeTennisTests
+{
+    [TestClass]
+    public class TennisTests
+    {
+        private readonly Tennis _tennis = new Tennis();
+
+        [TestMethod]
+        public void love_all()
+        {
+            ScoreShouldBe("love all");
+        }
+
+        [TestMethod]
+        public void fifteen_love()
+        {
+            GivenFirstPlayerScore(1);
+            ScoreShouldBe("fifteen love");
+        }
+
+
+        [TestMethod]
+        public void thirty_love()
+        {
+            GivenFirstPlayerScore(2);
+            ScoreShouldBe("thirty love");
+        }
+
+        [TestMethod]
+        public void forty_love()
+        {
+            GivenFirstPlayerScore(3);
+            ScoreShouldBe("forty love");
+        }
+
+        [TestMethod]
+        public void love_fifteen()
+        {
+            GivenSecondPlayerScore(1);
+            ScoreShouldBe("love fifteen");
+        }
+
+
+        [TestMethod]
+        public void love_thirty()
+        {
+            GivenSecondPlayerScore(2);
+            ScoreShouldBe("love thirty");
+        }
+
+
+        [TestMethod]
+        public void love_forty()
+        {
+            GivenSecondPlayerScore(3);
+            ScoreShouldBe("love forty");
+        }
+
+
+        [TestMethod]
+        public void fifteen_all()
+        {
+            GivenFirstPlayerScore(1);
+            GivenSecondPlayerScore(1);
+            ScoreShouldBe("fifteen all");
+        }
+
+
+        [TestMethod]
+        public void thirty_all()
+        {
+            GivenFirstPlayerScore(2);
+            GivenSecondPlayerScore(2);
+            ScoreShouldBe("thirty all");
+        }
+
+
+        [TestMethod]
+        public void deuce_3_3()
+        {
+            GivenDeuce();
+            ScoreShouldBe("deuce");
+        }
+
+
+        [TestMethod]
+        public void deuce_4_4()
+        {
+            GivenDeuce();
+            GivenFirstPlayerScore(1);
+            GivenSecondPlayerScore(1);
+            ScoreShouldBe("deuce");
+        }
+
+        [TestMethod]
+        public void first_player_adv()
+        {
+            GivenDeuce();
+            GivenFirstPlayerScore(1);
+            ScoreShouldBe("Sam adv");
+        }
+
+        [TestMethod]
+        public void first_player_win()
+        {
+            GivenDeuce();
+            GivenFirstPlayerScore(2);
+            ScoreShouldBe("Sam win");
+        }
+
+        [TestMethod]
+        public void second_player_adv()
+        {
+            GivenDeuce();
+            GivenSecondPlayerScore(1);
+            ScoreShouldBe("Joe adv");
+        }
+
+
+        [TestMethod]
+        public void second_player_win()
+        {
+            GivenDeuce();
+            GivenSecondPlayerScore(2);
+            ScoreShouldBe("Joe win");
+        }
+
+
+        private void GivenDeuce()
+        {
+            GivenFirstPlayerScore(3);
+            GivenSecondPlayerScore(3);
+        }
+
+        private void ScoreShouldBe(string expected)
+        {
+            var actual = _tennis.Score();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        private void GivenSecondPlayerScore(int times)
+        {
+            for (var i = 0; i < times; i++) _tennis.SecondPlayerScore();
+        }
+
+        private void GivenFirstPlayerScore(int times)
+        {
+            for (var i = 0; i < times; i++) _tennis.FirstPlayerScore();
+        }
+    }
+}
